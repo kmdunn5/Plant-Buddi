85623801c69c5b2a6c4565e8538715240c120b1e
kmdunn5 <kmdunn5@gmail.com>
2021-03-16 10:09:17 -0400

    working on user schema...why isn't it a schema yetgit add .

--- plant_buddi_front_end/src/App.js
+++ plant_buddi_front_end/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import plants from '../../plant-buddi-backend/models/Plant.js'
 
 let baseURL = 'http://localhost:3003'
 
@@ -7,9 +6,8 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      plants: plants
+      plants: []
     }
-    this.getPlants = this.getPlants.bind(this)
   }
   componentDidMount(){
     this.getPlants()
@@ -19,7 +17,10 @@ class App extends React.Component {
       .then(data => {
         return data.json()},
         err => console.log(err))
-      .then(parsedData => console.log(parsedData),
+      .then(parsedData => 
+        this.setState({
+          plants: parsedData
+        }),
        err=> console.log(err))
   }
 
@@ -27,16 +28,18 @@ class App extends React.Component {
     return (
       <div className='container'>
        <h1>Plant Buddi</h1>
-         {this.state.plants.map(plant => {
+          {this.state.plants.map(plant => {
            return (
-           <div key={plants._id}>  
-              <h3> {plants.commonName} </h3>
-              <h3> {plants.scientificName} </h3>
+            <div key={plant._id}>  
+              <h3> {plant.commonName} </h3>
+              <h3> {plant.scientificName} </h3>
             </div>
-           )}
-         )}
-         </div>
-         )}
+            )
+           })}
+      </div>
+    )
+  }    
+}
         
 
 
