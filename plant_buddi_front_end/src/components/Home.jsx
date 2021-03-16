c5cd6b6ba820fa6ca781984550cbbbadcb53a577
kmdunn5 <kmdunn5@gmail.com>
2021-03-16 17:07:13 -0400

    added a basic home page

--- plant_buddi_front_end/src/components/Home.jsx
+++ plant_buddi_front_end/src/components/Home.jsx
@@ -4,7 +4,8 @@ class Home extends Component {
     render() {
         return(
             <div>
-                
+                <h2>Sign In</h2>
+                <h2>Sign Up</h2>
             </div>
         )
     }
