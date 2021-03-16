881237a6e33937238a53fe6ed944fdfe5e38ed54
kmdunn5 <kmdunn5@gmail.com>
2021-03-16 16:38:01 -0400

    added react router logic into the index.js and App.js

--- plant_buddi_front_end/src/App.js
+++ plant_buddi_front_end/src/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react'
-// import { Route, Router, Switch } from 'react-router-dom'
+import { Route, Router, Switch } from 'react-router-dom'
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
 import Plants from './components/Plants.jsx'
+import Login from './components/Login'
 
 
 class App extends Component {
 
   render() {
     return (
-      <div>
-        < Header />
-        < Plants />
-        < Footer />
-      </div>
+      <Switch>
+        <Route exact path='/'>  
+          <div>
+            < Header />
+            < Plants />
+            < Footer />
+          </div>
+        </Route>
+        <Route path='/login'>
+          <Login />
+        </Route>
+      </Switch>
     )
   }    
 }
